0b77f46a5564a40d3cca8504a6380dc9d693c862
Azeez Sulaimon <111203778+sulaimonazeez@users.noreply.github.com>
2025-05-04 13:11:46 +0100

    Update downNav.jsx

--- src/component/downNav.jsx
+++ src/component/downNav.jsx
@@ -6,25 +6,25 @@ const DownNav = () => {
     const navigate = useNavigate();
     const Logout = () => {
         localStorage.removeItem("access_token");
-            localStorage.removeItem("refresh_token");
-            navigate("/login");
+        localStorage.removeItem("refresh_token");
+        navigate("/login");
       }
       
     return (
         <div className="bottom-nav">
-    <a href="/home" className="active">
+    <a onClick={()=> navigate("/home");} className="active">
       <i className="fas fa-home"></i>
       Home
     </a>
-    <a href="/history">
+    <a onClick={()=> navigate("/history");}>
       <i className="fas fa-list-alt"></i>
       History
     </a>
-    <a href="/notification">
+    <a onClick={()=> navigate("/notification");}>
       <i className="fas fa-bell"></i>
       Notification
     </a>
-    <a href="/logout">
+    <a onClick={Logout}>
       <i className="fas fa-sign-out-alt"></i>
       Logout
     </a>
