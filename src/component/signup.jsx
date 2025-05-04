d2e8d5f2346f2d0d5a18bef3ae55fa200467f642
Azeez Sulaimon <111203778+sulaimonazeez@users.noreply.github.com>
2025-05-04 11:23:55 +0100

    Update signup.jsx

--- src/component/signup.jsx
+++ src/component/signup.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import "./css/signup.css";
 
 const Signup = () => {
   const [username, setUsername] = useState('');
