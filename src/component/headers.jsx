efe0da30264d09cbf5cf85237281bd0cb16304ac
Azeez Sulaimon <111203778+sulaimonazeez@users.noreply.github.com>
2025-05-04 13:14:58 +0100

    Update headers.jsx

--- src/component/headers.jsx
+++ src/component/headers.jsx
@@ -155,26 +155,8 @@ const Header = () => {
       </div>
     </div>
   </div>
-
-  <div className="bottom-nav">
-    <a href="/home" className="active">
-      <i className="fas fa-home"></i>
-      Home
-    </a>
-    <a href="/history">
-      <i className="fas fa-list-alt"></i>
-      History
-    </a>
-    <a href="/notification">
-      <i className="fas fa-bell"></i>
-      Notification
-    </a>
-    <a href="/logout">
-      <i className="fas fa-sign-out-alt"></i>
-      Logout
-    </a>
+     <DownNav />
   </div>
-    </div>
   );
 };
 
