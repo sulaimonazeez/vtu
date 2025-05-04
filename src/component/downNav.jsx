34dfb7faf8afae8045bb6b2f6b8cfe35e2e9ca47
Azeez Sulaimon <111203778+sulaimonazeez@users.noreply.github.com>
2025-05-04 13:22:49 +0100

    Update downNav.jsx

--- src/component/downNav.jsx
+++ src/component/downNav.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import "./dashed.css";
 
 const DownNav = () => {
@@ -8,28 +8,28 @@ const DownNav = () => {
         localStorage.removeItem("access_token");
         localStorage.removeItem("refresh_token");
         navigate("/login");
-      }
-      
+    }
+
     return (
         <div className="bottom-nav">
-    <a onClick={()=> navigate("/home");} className="active">
-      <i className="fas fa-home"></i>
-      Home
-    </a>
-    <a onClick={()=> navigate("/history");}>
-      <i className="fas fa-list-alt"></i>
-      History
-    </a>
-    <a onClick={()=> navigate("/notification");}>
-      <i className="fas fa-bell"></i>
-      Notification
-    </a>
-    <a onClick={Logout}>
-      <i className="fas fa-sign-out-alt"></i>
-      Logout
-    </a>
-  </div>
-    )
+            <Link to="/home" className="nav-link">
+                <i className="fas fa-home"></i>
+                Home
+            </Link>
+            <Link to="/history" className="nav-link">
+                <i className="fas fa-list-alt"></i>
+                History
+            </Link>
+            <Link to="/notification" className="nav-link">
+                <i className="fas fa-bell"></i>
+                Notification
+            </Link>
+            <a onClick={Logout} className="nav-link">
+                <i className="fas fa-sign-out-alt"></i>
+                Logout
+            </a>
+        </div>
+    );
 }
 
 export default DownNav;
