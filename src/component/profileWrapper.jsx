f5b496d5bf0b5bf4f4502d0ab3e577d14cd14628
Azeez Sulaimon <111203778+sulaimonazeez@users.noreply.github.com>
2025-05-08 17:06:23 +0100

    Update profileWrapper.jsx

--- src/component/profileWrapper.jsx
+++ src/component/profileWrapper.jsx
@@ -1,1 +1,19 @@
+import React from "react";
+import Profile from "./profile.jsx";
 
+const ProfileWrapper = () => {
+    const user = {
+        username: 'john_doe',
+        email: 'john.doe@example.com',
+        firstName: 'John',
+        lastName: 'Doe',
+        phone: '+2348012345678'
+    };
+    return (
+       <div>
+           <Profile user={user} />
+       </div>
+    )
+}
+
+export default ProfileWrapper;
