ea94fc6693230a902e5ed98fefcaef8dd5adeef1
Azeez Sulaimon <111203778+sulaimonazeez@users.noreply.github.com>
2025-05-11 11:14:28 +0100

    Update profileWrapper.jsx

--- src/component/profileWrapper.jsx
+++ src/component/profileWrapper.jsx
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Profile from "./profile.jsx";
-
+import axiosInstance from "./utility";
 const ProfileWrapper = () => {
-    const user = {
-        username: 'john_doe',
-        email: 'john.doe@example.com',
-        firstName: 'John',
-        lastName: 'Doe',
-        phone: '+2348012345678'
-    };
+    const [user, setUser] = useState({})
+
+    const fetchProfile = async () => {
+        try {
+          let response = await axiosInstance.get("/profile/");
+          setUser(response.data);
+        } catch {
+           console.log("unable to fetch profile");
+        }
+                  
+    }
+    useEffect(() =>{
+       fetchProfile();
+    }, []);
     return (
        <div>
            <Profile user={user} />
